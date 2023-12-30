8fa9e8cd08ff9272d587b01f31e15344da59f73f
TechMi97 <mdtayub97@gmail.com>
2023-12-31 00:26:40 +0800

    Tasks being displayed with map

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+const Button = ({color, text, onClick}) => {
+   
+
+
   return (
-    <button  style={{backgroundColor:color}}
+    <button onClick= {onClick}  style={{backgroundColor:color}}
     className='btn'>
         {text}
         </button>
@@ -16,6 +19,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func.isRequired
 }
 
 export default Button
