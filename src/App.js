896a69264506d592b3eac6a22de8fba21b657525
TechMi97 <mdtayub97@gmail.com>
2024-01-06 20:44:27 +0800

    Till toggle button before yarn build at 1.19.27

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 import { useState } from "react"
 const  App = () => { 
+  const [showAddTask, setShowAddTask] = useState
+  (false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -26,6 +30,17 @@ const  App = () => {
     ]
 )
 
+
+//Add Task
+
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+
+
 //Delete Task
 
 const deleteTask = (id) => {
@@ -40,7 +55,9 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-     <Header />
+     <Header onAdd={() => setShowAddTask
+      (!showAddTask)} showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder}/>:
      'No Tasks To show'}
     </div>
