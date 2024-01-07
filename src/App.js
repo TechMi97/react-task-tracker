6d22e620d12c2a656a121004fad5ccf9d469919b
TechMi97 <mdtayub97@gmail.com>
2024-01-07 12:19:48 +0800

    Route, use Location, add button conditional statement, no refresh when click go back

--- src/App.js
+++ src/App.js
@@ -95,13 +95,21 @@ const toggleReminder = async (id) => {
     <div className="container">
      <Header onAdd={() => setShowAddTask
       (!showAddTask)} showAdd={showAddTask}/>
-     {showAddTask && <AddTask onAdd={addTask}/>}
-     {tasks.length > 0 ? ( 
-     <Tasks tasks={tasks} 
-     onDelete={deleteTask} 
-     onToggle = {toggleReminder}/>):
-     ('No Tasks To show')}
       <Routes>
+     <Route path='/' element={
+       <>
+       {showAddTask && <AddTask onAdd={addTask} />}
+       {tasks.length > 0 ? (
+         <Tasks
+           tasks={tasks}
+           onDelete={deleteTask}
+           onToggle={toggleReminder}
+         />
+       ) : (
+         'No Tasks To Show'
+       )}
+     </>
+     }/> 
      <Route path='/about' element={<About/>}/>
      </Routes>
      <Footer/>
